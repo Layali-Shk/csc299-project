01b0eee1ff0e14034aff9f2d63da82e5cc72edce
Layali-Shk <lshkokan@depaul.edu>
2025-11-05 15:11:17 -0600

    Update notes_manager.py

--- tasks3/src/notes_manager.py
+++ tasks3/src/notes_manager.py
@@ -1,1 +1,10 @@
+class NotesManager:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def list_notes(self):
+        return self.notes
 
