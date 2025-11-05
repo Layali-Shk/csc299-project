12e50090d510a8932ca0196f97eb47abfa8f2f5a
Layali-Shk <lshkokan@depaul.edu>
2025-11-05 15:10:51 -0600

    Update test_tasks_manager.py

--- tasks3/tests/test_tasks_manager.py
+++ tasks3/tests/test_tasks_manager.py
@@ -1,1 +1,13 @@
 
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, title):
+        self.tasks.append({"title": title, "completed": False})
+
+    def complete_task(self, index):
+        self.tasks[index]["completed"] = True
+
+    def run(self):
+        print("TaskManager running. Number of tasks:", len(self.tasks))
