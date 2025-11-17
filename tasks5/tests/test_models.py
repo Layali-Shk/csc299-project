ebae4aa3b6e0c4838b29167d3f6ac33518eee027
Layali-Shk <lshkokan@depaul.edu>
2025-11-17 15:49:28 -0600

    Update test_models.py

--- tasks5/tests/test_models.py
+++ tasks5/tests/test_models.py
@@ -1,1 +1,13 @@
+from src.models import Task
+
+def test_task_creation():
+    t = Task(1, "Study")
+    assert t.id == 1
+    assert t.title == "Study"
+    assert not t.completed
+
+def test_task_complete():
+    t = Task(1, "Study")
+    t.complete()
+    assert t.completed
 
