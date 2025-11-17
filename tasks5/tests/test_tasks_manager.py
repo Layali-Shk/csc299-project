c4a9edd4681926a85603397ba678d1ae7ab9a387
Layali-Shk <lshkokan@depaul.edu>
2025-11-17 15:49:44 -0600

    Update test_tasks_manager.py

--- tasks5/tests/test_tasks_manager.py
+++ tasks5/tests/test_tasks_manager.py
@@ -1,1 +1,19 @@
+from src.tasks_manager import TasksManager
+
+def test_add_task():
+    tm = TasksManager()
+    t = tm.add_task("Do homework")
+    assert t.title == "Do homework"
+    assert t.id == 1
+
+def test_complete_task():
+    tm = TasksManager()
+    tm.add_task("Clean")
+    assert tm.complete_task(1)
+
+def test_delete_task():
+    tm = TasksManager()
+    tm.add_task("Sleep")
+    tm.delete_task(1)
+    assert len(tm.list_tasks()) == 0
 
