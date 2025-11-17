2a4de6b9c7110b639f33f78d4b3f34cda552398b
Layali-Shk <lshkokan@depaul.edu>
2025-11-17 15:49:01 -0600

    Update models.py

--- tasks5/src/models.py
+++ tasks5/src/models.py
@@ -1,1 +1,9 @@
+class Task:
+    def __init__(self, id: int, title: str):
+        self.id = id
+        self.title = title
+        self.completed = False
+
+    def complete(self):
+        self.completed = True
 
