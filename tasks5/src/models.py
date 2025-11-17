a6c9f99b88c53428c6c02e2b8271d014a480e0f4
Layali-Shk <lshkokan@depaul.edu>
2025-11-17 17:47:19 -0600

    Update models.py

--- tasks5/src/models.py
+++ tasks5/src/models.py
@@ -1,3 +1,4 @@
+# src/models.py
 class Task:
     def __init__(self, id, title, completed=False):
         self.id = id
@@ -6,14 +7,3 @@ class Task:
 
     def complete(self):
         self.completed = True
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "completed": self.completed
-        }
-
-    @staticmethod
-    def from_dict(data):
-        return Task(data['id'], data['title'], data.get('completed', False))
