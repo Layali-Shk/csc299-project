a7719d8e101b3d8836a67444db7aa9da859d9f4c
Layali-Shk <lshkokan@depaul.edu>
2025-11-17 13:38:15 -0600

    Update main.py

--- tasks5/src/main.py
+++ tasks5/src/main.py
@@ -1,1 +1,11 @@
 
+from task_manager import TaskManager
+from utils import print_welcome
+
+def main():
+    print_welcome()
+    tm = TaskManager("memory/data.json")
+    tm.run()
+
+if __name__ == "__main__":
+    main()
