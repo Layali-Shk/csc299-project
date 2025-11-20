1c5c25c0a66a98a8396950d1c09e6ef452e85053
Layali-Shk <lshkokan@depaul.edu>
2025-11-19 22:02:09 -0600

    Update tasks.py

--- final_app/src/tasks/tasks.py
+++ final_app/src/tasks/tasks.py
@@ -1,1 +1,8 @@
+from utils import generate_id,current_time
 
+
+class Task:
+def __init__(self,title,description='',priority=3,due=None,tags=None,subtasks=None):
+self.id=generate_id(); self.title=title; self.description=description; self.priority=priority; self.due=due; self.tags=tags or [];
+self.subtasks=subtasks or []; self.completed=False; self.created_at=current_time()
+def to_dict(self): return vars(self)
