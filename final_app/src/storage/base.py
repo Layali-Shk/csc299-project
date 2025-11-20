c7624cda7465b340926e35354d77f231ac8e3966
Layali-Shk <lshkokan@depaul.edu>
2025-11-19 22:00:53 -0600

    Update base.py

--- final_app/src/storage/base.py
+++ final_app/src/storage/base.py
@@ -1,1 +1,14 @@
+from abc import ABC, abstractmethod
 
+
+class Storage(ABC):
+@abstractmethod
+def add_note(self, note): pass
+@abstractmethod
+def get_notes(self): pass
+@abstractmethod
+def add_task(self, task): pass
+@abstractmethod
+def get_tasks(self): pass
+@abstractmethod
+def update_task(self, task_id, **kwargs): pass
